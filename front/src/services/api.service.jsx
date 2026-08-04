337d1a4c7115af18c8818f031dcdd575084fd97c
Stjepanslo <90937675+Stjepanslo@users.noreply.github.com>
2026-08-04 19:40:14 -0300

    A ver ahora

--- front/src/services/api.service.jsx
+++ front/src/services/api.service.jsx
@@ -25,7 +25,7 @@ export function useApi() {
             }
         }
 
-        return fetch("https://backendfinal-1jr0.onrender.com/api" + uri, options)
+        return fetch("https://backendfinal-1jr6.onrender.com" + uri, options)
             .then(res => {
                 if (res.ok) return res.json()
                 if (res.status == 401) navigate("/login")
